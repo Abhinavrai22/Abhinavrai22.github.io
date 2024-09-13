9e8e08cf533c5434ad43b83729e1d1751779a155
Abhinavrai22 <93989345+Abhinavrai22@users.noreply.github.com>
2024-09-13 17:58:29 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// import React from "react";
-// import ReactDom from 'react-dom/client';
-// import App from "./ClassComponent/App";
-
-
-
-// const root = ReactDom.createRoot(document.getElementById('root'));
-// root.render(
-// <>
-// <App/>
-// </>
-// );
-
-
-
 import React from "react";
 import ReactDom from 'react-dom/client';
 import App from "./FunctionComponent/App"
